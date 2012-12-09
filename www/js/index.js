1574292814df428901209f03d4b481578e4471d6
Karl Svartholm <gorbiz@gmail.com>
2012-12-09 16:54:35 +0100

    Improved Javascript readability.

--- www/js/index.js
+++ www/js/index.js
@@ -1,23 +1,25 @@
-if (navigator.userAgent.match(/(iPhone|iPod|iPad)/)) {
-	document.addEventListener('deviceready', onDeviceReady, false);
-} else {
-	// for testing in a browser
-	$(document).ready(function() {
-		onDeviceReady();
-	});
+
+// start application when PhoneGap is ready
+document.addEventListener('deviceready', onDeviceReady, false);
+
+// for testing in browser
+if (!navigator.userAgent.match(/(iPhone|iPod|iPad)/)) {
+	$(document).ready(onDeviceReady);
 }
 
 function onDeviceReady() {
-	
-	var currentImageURI;
 	
 	// iScroll 4
 	var myScroll = new iScroll('wrapper');
 	document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
 
-	$('.make-wish .picture-frame').on('click', function(event) {
-	    event.preventDefault();
 
+
+	var currentImageURI;
+
+
+
+	$('.make-wish .picture-frame').on('click', function(event) {
 	    // for testing in a browser
 	    if (!window.plugins || !window.plugins.actionSheet) {
 	    	onSuccessAddedPhoto('http://placekitten.com/300/300');
@@ -29,54 +31,46 @@ function onDeviceReady() {
 	        null,
 	        ['Take Photo', 'Choose Existing', 'Cancel'],
 	        function(buttonValue, buttonIndex) {
-	            switch (arguments[1]) {
-	                case 0:
-	                    getPhoto(Camera.PictureSourceType.CAMERA, onSuccessAddedPhoto);
-	                    break;
-	                case 1:
-	                    getPhoto(Camera.PictureSourceType.PHOTOLIBRARY, onSuccessAddedPhoto);
-	                    break;
-	            }
+	        	if (buttonIndex == 0)  {
+	        		getPhoto(Camera.PictureSourceType.CAMERA, onSuccessAddedPhoto);
+	        	} else if (buttonIndex == 1)  {
+	        		getPhoto(Camera.PictureSourceType.PHOTOLIBRARY, onSuccessAddedPhoto);
+	        	}
 	        },
 	        { cancelButtonIndex: 2 }
 	    );
 	});
-	
+
 	$('.wishes li img').live('click', function(event) {
-	    event.preventDefault();
-	
 		var self = this;
 	    function onSuccessChangedPhoto(imageURI) {
 			$(self).attr('src', imageURI);
 		}
-	    
+
 	    // for testing in a browser
 	    if (!window.plugins || !window.plugins.actionSheet) {
 	    	onSuccessChangedPhoto('http://placekitten.com/300/300');
 	    	return;
 	    }
-	    
+
 	    // summon dialog to allow user to take photo or choose photo from library
 	    window.plugins.actionSheet.create(
 	        null,
 	        ['Take Photo', 'Choose Existing', 'Cancel'],
 	        function(buttonValue, buttonIndex) {
-	            switch (arguments[1]) {
-	                case 0:
-	                    getPhoto(Camera.PictureSourceType.CAMERA, onSuccessChangedPhoto);
-	                    break;
-	                case 1:
-	                    getPhoto(Camera.PictureSourceType.PHOTOLIBRARY, onSuccessChangedPhoto);
-	                    break;
-	            }
+	        	if (buttonIndex == 0)  {
+	        		getPhoto(Camera.PictureSourceType.CAMERA, onSuccessChangedPhoto);
+	        	} else if (buttonIndex == 1)  {
+	        		getPhoto(Camera.PictureSourceType.PHOTOLIBRARY, onSuccessChangedPhoto);
+	        	}
 	        },
 	        { cancelButtonIndex: 2 }
 	    );
 	});
-	
-	
-	
-	function saveItem(event) {
+
+
+
+	$('form.make-wish').submit(function(event) {
 	    event.preventDefault();
 	    var currentText = $('.make-wish textarea').val();
 	    if (!currentImageURI && !currentText) return;
@@ -98,7 +92,6 @@ function onDeviceReady() {
 	    }
 	
 	    item += '<div class="description"><h2>' + header + '</h2>'
-	    	// TODO Perhaps don't show if not present...
 	    	+ '<p>' + text + '</p></div>'
 	        + '<button class="remove"></button>'
 	        + '<div style="clear:left;">';
@@ -110,9 +103,8 @@ function onDeviceReady() {
 	    $('.make-wish textarea').val('');
 	    currentImageURI = '';
 	    $('.make-wish .picture-frame img').remove();
-	}
+	});
 	
-	$('form.make-wish').submit(saveItem);
 	
 	$('.wishes button.remove').live('click', function() {
 	    $(this).parent().remove();
