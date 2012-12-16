7a44284a2310d6d3fe31fb4b18fb0e214cfc1f33
Karl Svartholm <gorbiz@gmail.com>
2012-12-16 12:22:50 +0100

    Fixed all javascript warnings.

--- www/js/index.js
+++ www/js/index.js
@@ -4,123 +4,123 @@ document.addEventListener('deviceready', onDeviceReady, false);
 
 // for testing in browser
 if (!navigator.userAgent.match(/(iPhone|iPod|iPad)/)) {
-	$(document).ready(onDeviceReady);
+    $(document).ready(onDeviceReady);
 }
 
 function onDeviceReady() {
-	
-	// iScroll 4
-	var myScroll = new iScroll('wrapper');
-	document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
+
+    // iScroll 4
+    var myScroll = new iScroll('wrapper');
+    document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
 
 
 
-	var currentImageURI;
+    var currentImageURI;
 
-	// add image from "make-wish"
-	$('.make-wish .picture-frame').on('click',
-		allowUserToAddPicture(
-			function(imageURI, context) {
-			    currentImageURI = imageURI;
-				$(context).find('img').remove();
-				$(context).append('<img src="' + currentImageURI + '" />');
-			}
-		)
-	);
+    // add image from "make-wish"
+    $('.make-wish .picture-frame').on('click',
+        allowUserToAddPicture(
+            function(imageURI, context) {
+                currentImageURI = imageURI;
+                $(context).find('img').remove();
+                $(context).append('<img src="' + currentImageURI + '" />');
+            }
+        )
+    );
 
-	function allowUserToAddPicture(onSuccess) {
-		
-		// communicate with iPhone - get picture
-		function getPhoto(sourceType, onSuccessCallback) {
-		    navigator.camera.getPicture(onSuccessCallback, onFail, {
-		        quality: 50,
-		        sourceType: sourceType,
-		        destinationType: Camera.DestinationType.FILE_URI }); 
-		
-		    function onFail(message) {}
-		}
+    function allowUserToAddPicture(onSuccess) {
 
-		return function(event) {
-			
-			var _this = this;
-			function innerSuccess(imageURI) {
-			  onSuccess(imageURI, _this);
-			};
+        // communicate with iPhone - get picture
+        function getPhoto(sourceType, onSuccessCallback) {
+            navigator.camera.getPicture(onSuccessCallback, onFail, {
+                quality: 50,
+                sourceType: sourceType,
+                destinationType: Camera.DestinationType.FILE_URI });
 
-		    // for testing in a browser
-		    if (!window.plugins || !window.plugins.actionSheet) {
-		    	innerSuccess('http://placekitten.com/300/300');
-		    	return;
-		    }
+            function onFail(message) {}
+        }
 
-		    // summon dialog to allow user to take photo or choose photo from library
-		    window.plugins.actionSheet.create(
-		        null,
-		        ['Take Photo', 'Choose Existing', 'Cancel'],
-		        function(buttonValue, buttonIndex) {
-		        	if (buttonIndex == 0)  {
-		        		getPhoto(Camera.PictureSourceType.CAMERA, innerSuccess);
-		        	} else if (buttonIndex == 1)  {
-		        		getPhoto(Camera.PictureSourceType.PHOTOLIBRARY, innerSuccess);
-		        	}
-		        },
-		        { cancelButtonIndex: 2 }
-		    );
-		}
-	}
+        return function(event) {
+
+            var _this = this;
+            function innerSuccess(imageURI) {
+              onSuccess(imageURI, _this);
+            }
+
+            // for testing in a browser
+            if (!window.plugins || !window.plugins.actionSheet) {
+                innerSuccess('http://placekitten.com/300/300');
+                return;
+            }
+
+            // summon dialog to allow user to take photo or choose photo from library
+            window.plugins.actionSheet.create(
+                null,
+                ['Take Photo', 'Choose Existing', 'Cancel'],
+                function(buttonValue, buttonIndex) {
+                    if (buttonIndex === 0)  {
+                        getPhoto(Camera.PictureSourceType.CAMERA, innerSuccess);
+                    } else if (buttonIndex === 1)  {
+                        getPhoto(Camera.PictureSourceType.PHOTOLIBRARY, innerSuccess);
+                    }
+                },
+                { cancelButtonIndex: 2 }
+            );
+        };
+    }
 
 
-	
-	// make a wish
-	
-	$('form.make-wish').submit(function(event) {
-	    event.preventDefault();
-	    var currentText = $(this).find('textarea').val();
-	    if (!currentImageURI && !currentText) return;
 
-		var header = '', text = '';
-		if (currentText) {
-			var textSections = currentText.split("\n");
-			header = textSections[0];
-			if (textSections.length > 1) text = textSections.splice(1).join("\n");
-			
-			if (!currentImageURI) {
-				currentImageURI = 'img/placeholder-photo.png';
-			}
-		}
+    // make a wish
 
-	    var wish = '<li>';
-	    if (currentImageURI) {
-	        wish += '<img src="' + currentImageURI + '" />';    
-	    }
+    $('form.make-wish').submit(function(event) {
+        event.preventDefault();
+        var currentText = $(this).find('textarea').val();
+        if (!currentImageURI && !currentText) return;
 
-	    wish += '<div class="description"><h2>' + header + '</h2>'
-	    	+ '<p>' + text + '</p></div>'
-	        + '<button class="remove"></button>'
-	        + '<div style="clear:left;">';
-	    
-	    wish += '</li>';
+        var header = '', text = '';
+        if (currentText) {
+            var textSections = currentText.split("\n");
+            header = textSections[0];
+            if (textSections.length > 1) text = textSections.splice(1).join("\n");
 
-		// make remove button work
-		wish = $(wish);
-		wish.find('button.remove').click(function() {
-			$(this).parent().remove();
-		    myScroll.refresh();
-		});
+            if (!currentImageURI) {
+                currentImageURI = 'img/placeholder-photo.png';
+            }
+        }
 
-		// change image on wishes
-		wish.find('img').click(
-			allowUserToAddPicture(
-				function(imageURI, context) {
-					$(context).attr('src', imageURI);
-				}
-			)
-		);
+        var wish = '<li>';
+        if (currentImageURI) {
+            wish += '<img src="' + currentImageURI + '" />';
+        }
 
-		$('.wishes').append(wish);
-		myScroll.refresh();
-		$('.make-wish textarea').val('');
-		currentImageURI = '';
-		$('.make-wish .picture-frame img').remove();
-	});
+        wish += '<div class="description"><h2>' + header + '</h2>';
+        wish += '<p>' + text + '</p></div>';
+        wish += '<button class="remove"></button>';
+        wish += '<div style="clear:left;">';
+
+        wish += '</li>';
+
+        // make remove button work
+        wish = $(wish);
+        wish.find('button.remove').click(function() {
+            $(this).parent().remove();
+            myScroll.refresh();
+        });
+
+        // change image on wishes
+        wish.find('img').click(
+            allowUserToAddPicture(
+                function(imageURI, context) {
+                    $(context).attr('src', imageURI);
+                }
+            )
+        );
+
+        $('.wishes').append(wish);
+        myScroll.refresh();
+        $('.make-wish textarea').val('');
+        currentImageURI = '';
+        $('.make-wish .picture-frame img').remove();
+    });
 }
